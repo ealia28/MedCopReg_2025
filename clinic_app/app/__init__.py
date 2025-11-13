01b9f2b9f0585f43da34c7ac0fbaca58b2c42cb2
ja1x <121609867+ja1x@users.noreply.github.com>
2025-11-14 00:10:03 +0300

    Update __init__.py

--- clinic_app/app/__init__.py
+++ clinic_app/app/__init__.py
@@ -7,16 +7,20 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    
-    app.config.from_object('config.Config')  # Загружаем конфиг
+    app.config.from_object('config.Config')
     
     db.init_app(app)
     migrate.init_app(app, db)
-    # Импортируем модели, чтобы SQLAlchemy знал про них
-    from app import models  
+    
+    from app import models
 
-    # Подключаем маршруты
+    with app.app_context():
+        # Это создаст все таблисы автоматически
+        db.create_all()
+        print("✅ Database tables created successfully")
+
     from app.routes import main_bp
     app.register_blueprint(main_bp)
 
     return app
+

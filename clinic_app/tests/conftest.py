fb00dbf7f7eb337ec97ebf8417df7057a7084ad4
ja1x <121609867+ja1x@users.noreply.github.com>
2025-12-09 19:40:31 +0300

    Update conftest.py

--- clinic_app/tests/conftest.py
+++ clinic_app/tests/conftest.py
@@ -9,21 +9,15 @@ from app import create_app, db
 
 @pytest.fixture
 def app():
-    app = create_app()
-    app.config.update(
-        TESTING=True,
-        SQLALCHEMY_DATABASE_URI="sqlite:///:memory:",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    )
-    with app.app_context():
-        db.create_all()
-        yield app
-        db.session.remove()
-        db.drop_all()
+    os.environ["FLASK_ENV"] = "testing"
+    app = create_app(testing=True)
+    return app
+
 
 @pytest.fixture
 def client(app):
     return app.test_client()
+
 
 
 

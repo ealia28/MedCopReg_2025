fe0bb2835d73d26c17667850da8a8be026740857
ealia28 <145836658+ealia28@users.noreply.github.com>
2025-12-09 18:34:51 +0300

    Update conftest.py

--- clinic_app/tests/conftest.py
+++ clinic_app/tests/conftest.py
@@ -10,11 +10,11 @@ from app import create_app, db
 @pytest.fixture
 def app():
     app = create_app()
-    app.config.update({
-        "TESTING": True,
-        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+    app.config.update(
+        TESTING=True,
+        SQLALCHEMY_DATABASE_URI="sqlite:///:memory:",
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    })
+    )
     with app.app_context():
         db.create_all()
         yield app
@@ -24,6 +24,7 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
 
 
 

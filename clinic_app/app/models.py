14f5919a6187cc49cd084819fc06ca32a7ccc1c2
Ilya Abakaev <ilya.abakaev.30@gmail.com>
2025-12-09 12:17:46 -0500

    BROKEN: make patient.name nullable to break tests

--- clinic_app/app/models.py
+++ clinic_app/app/models.py
@@ -2,7 +2,7 @@ from app import db
 
 class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150), nullable=False)
+    name = db.Column(db.String(150), nullable=True) # специально ломаем (должно быть False)
     gender = db.Column(db.String(10), nullable=False)
     birth_date = db.Column(db.Date, nullable=False)
     address = db.Column(db.String(255), nullable=False)

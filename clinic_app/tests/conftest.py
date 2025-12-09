77502069de16104daacaea04b9eb69504dc11611
ja1x <121609867+ja1x@users.noreply.github.com>
2025-12-09 19:47:50 +0300

    Update conftest.py

--- clinic_app/tests/conftest.py
+++ clinic_app/tests/conftest.py
@@ -1,33 +1,27 @@
-import sys
-import os
 import pytest
-
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.insert(0, BASE_DIR)
-
 from app import create_app, db
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def app():
-    os.environ["FLASK_ENV"] = "testing"
     app = create_app(testing=True)
-    return app
+
+    # Application context is needed for DB operations
+    with app.app_context():
+        yield app
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def client(app):
     return app.test_client()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+@pytest.fixture(scope="function")
+def db_session(app):
+    """
+    Provides a clean database for each test.
+    """
+    with app.app_context():
+        db.create_all()
+        yield db
+        db.session.remove()
+        db.drop_all()

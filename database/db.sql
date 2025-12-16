c28b88e6528000b6238803afca4d513d91df5701
ja1x <121609867+ja1x@users.noreply.github.com>
2025-12-16 21:10:11 +0300

    Update db.sql

--- database/db.sql
+++ database/db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE patient (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(150) NOT NULL,
+    full_name VARCHAR(150) NOT NULL,
     gender VARCHAR(10) NOT NULL,
     birth_date DATE NOT NULL,
     address VARCHAR(255) NOT NULL,
@@ -9,13 +9,23 @@ CREATE TABLE patient (
 
 CREATE TABLE doctor (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(150) NOT NULL,
+    full_name VARCHAR(150) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE medicine (
+    id INT NOT NULL AUTO_INCREMENT,
+    medicine_name VARCHAR(100) NOT NULL,
+    usage_description TEXT NOT NULL,
+    action_description TEXT NOT NULL,
+    side_effects TEXT NOT NULL,
+    UNIQUE KEY uq_medicine_name (medicine_name),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE appointment (
     id INT NOT NULL AUTO_INCREMENT,
-    date DATE NOT NULL,
+    appointment_date DATE NOT NULL,
     location VARCHAR(100) NOT NULL,
     symptoms TEXT NOT NULL,
     diagnosis VARCHAR(150) NOT NULL,
@@ -23,12 +33,12 @@ CREATE TABLE appointment (
     patient_id INT NOT NULL,
     doctor_id INT NOT NULL,
     PRIMARY KEY (id),
-    KEY (patient_id),
-    KEY (doctor_id),
-    CONSTRAINT fk_patient
+    KEY idx_appointment_patient (patient_id),
+    KEY idx_appointment_doctor (doctor_id),
+    CONSTRAINT fk_appointment_patient
         FOREIGN KEY (patient_id)
         REFERENCES patient (id),
-    CONSTRAINT fk_doctor
+    CONSTRAINT fk_appointment_doctor
         FOREIGN KEY (doctor_id)
         REFERENCES doctor (id)
 );
